265ff2806607f8df67e3f39b2046d7950847a8dd
eli <elixircist@gmail.com>
2017-10-14 16:57:24 -0400

    adding endpoints and manual

--- back/endpoints/createExam.php
+++ back/endpoints/createExam.php
@@ -3,6 +3,11 @@
 	$body = json_decode(file_get_contents('php://input'), true);
 	$question_id = $body['question_id'];
 	$exam_name = $body['exam_name'];
+	$exam_id = $body['exam_id'];
+
+	if($question_id == '' or $exam_name == ''){
+		die('');
+	}
 
 	$link = mysql_connect('sql.njit.edu', 'as2487', 'QOdKwfpVY');
 	
@@ -12,7 +17,14 @@
 	
 	mysql_select_db('as2487');
 	
-	$query = sprintf('INSERT INTO Exams (question_id, exam_name) VALUES ("%s", "%s");', $question_id, $exam_name);
+	if($exam_id == ''){
+		$query = sprintf('INSERT INTO Exams (question_id, exam_name) VALUES ("%s", "%s");', $question_id, $exam_name);
+	}
+	else{
+		$query = sprintf('INSERT INTO Exams (exam_id, question_id, exam_name) VALUES
+		("%s","%s","%s");' $exam_id, $question_id, $exam_name);
+	}
+
 	mysql_query($query);
 
 	echo mysql_insert_id();
